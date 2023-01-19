f59a539a70a24c413fd8821fa33d5e526eb236ab
Mihail Tudos <mihail..tudos@idwalmarine.com>
2023-01-19 22:30:06 +0000

    Completed Loan Calc

---
+++ CSharpMortgageCalculator/CSharpMortgageCalculator/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using CSharpMortgageCalculator.Helpers;
+using CSharpMortgageCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpMortgageCalculator.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult App()
+		{
+			Loan loan = new();
+			
+			loan.Payment = 0.0m;
+			loan.TotalInterest = 0.0m;
+			loan.TotalCost = 0.0m;
+			loan.Rate = 3.5m;
+			loan.Amount = 15000m;
+			loan.Term = 60;
+
+			return View(loan);
+		}
+
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		public IActionResult App(Loan loan)
+		{
+			//calculate monthly payment
+			var loanHelper = new LoanHelper();
+			Loan newLoan = loanHelper.GetPayment(loan);
+			//return the loan to the view 
+			return View(loan);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ CSharpMortgageCalculator/CSharpMortgageCalculator/Helpers/LoanHelper.cs
@@ -0,0 +1,69 @@
+﻿using CSharpMortgageCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpMortgageCalculator.Helpers
+{
+	public class LoanHelper
+	{
+		public Loan GetPayment(Loan loan)
+		{
+			loan.Payment = CalcPayment(loan.Amount, loan.Rate, loan.Term);
+			//create a loop 1 to therm 
+			//calculate payment schedule
+			var balance = loan.Amount;
+			var totalInterest = 0.0m;
+			var monthlyInterest = 0.0m;
+			var monthlyPrincipal = 0.0m;
+			var monthlyRate = CalcMonthlyRate(loan.Rate);
+
+			for (int month = 1; month <= loan.Term; month++)
+			{
+				monthlyInterest = CalculateMonthlyInterest(balance, monthlyRate);
+				totalInterest += monthlyInterest;
+				monthlyPrincipal = loan.Payment - monthlyInterest;
+				balance -= monthlyPrincipal;
+
+				LoanPayment loanPayment = new();
+				loanPayment.Month = month;
+				loanPayment.Payment = loan.Payment;
+				loanPayment.MonthlyPrincipal = monthlyPrincipal;
+				loanPayment.MonthlyInterest = monthlyInterest;
+				loanPayment.TotalInterest = totalInterest;
+				loanPayment.Balance = balance;
+
+				//push payments into the loan 
+				loan.Payments.Add(loanPayment);
+			}
+
+			loan.TotalInterest = totalInterest;
+			loan.TotalCost = loan.Amount + totalInterest;
+
+			return loan;
+		}
+
+		private decimal CalcPayment(decimal amount, decimal rate, int term)
+		{
+			decimal payment = 0.0m;
+			var rateD = Convert.ToDouble(CalcMonthlyRate(rate));
+			var amountD = Convert.ToDouble(amount);
+
+			var paymentD = (amountD * rateD) / (1 - Math.Pow(1 + rateD, -term));
+
+
+			return Convert.ToDecimal(paymentD);
+		}
+
+		private decimal CalcMonthlyRate(decimal rate)
+		{
+			return rate / 1200;
+		}
+
+		private decimal CalculateMonthlyInterest(decimal balance, decimal monthlyRate)
+		{
+			return balance * monthlyRate;
+		}
+	}
+}
